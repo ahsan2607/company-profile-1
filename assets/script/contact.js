95b459fcc7819e385fc1c58a619690d69fb58009
Ahsan <firstsulthangalaxias@gmail.com>
2025-01-01 18:51:27 +0700

    Change mail form

--- assets/script/contact.js
+++ assets/script/contact.js
@@ -1,7 +1,8 @@
-document.getElementById("input_mail").addEventListener("submit", (e) => {
-  const name = document.getElementById("input-mail-name").value
-  const subject = document.getElementById("input-mail-subject").value
-  const text = document.getElementById("input-mail-text").value
+document.getElementById("input_mail").addEventListener("change", (Event) => {
+  const name = document.getElementById("input-mail-name").value;
+  const subject = document.getElementById("input-mail-subject").value;
+  const text = document.getElementById("input-mail-text").value;
 
-  e.target.setAttribute("action", `mailto:plasticmji@gmail.com?subject=${subject}&body=Halo, nama saya ${name}. ${text}`)
-})
+  document.getElementById("input_mail").setAttribute("action", `mailto:plasticmji@gmail.com?subject=${subject}&body=Halo, nama saya ${name}. ${text}`);
+  // console.log(`mailto:plasticmji@gmail.com?subject=${subject}&body=Halo, nama saya ${name}. ${text}`)
+});
